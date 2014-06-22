e3138db04fe179b2b8cfd27770dabbc1247e689d
Elliot Hesp <elliot.hesp@gmail.com>
2014-06-22 23:47:10 +0100

    Added local files + header styling

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -2,13 +2,17 @@
 <html lang="en" ng-app>
 <head>
 	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<title>SSMS</title>
 
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.18/angular.min.js"></script>
+  [[ HTML::style('css/bootstrap.min.css') ]]
+  [[ HTML::style('css/font-awesome.min.css') ]]
+  [[ HTML::style('css/dashboard.min.css') ]]
+  [[ HTML::style('css/content.min.css') ]]
 
-	[[ HTML::style('css/dashboard.min.css') ]]
+  [[ HTML::script('js/angular.min.js') ]]
+  [[ HTML::script('js/ng-bootstrap.min.js') ]]
 
 </head>
 <body>
@@ -27,20 +31,20 @@
           <span>NAVIGATION</span>
         </li>
         <li class="sidebar-list">
-          <a href="#">Dashboard <span class="menu-icon glyphicon glyphicon-dashboard"></span></a>
+          <a href="#">Dashboard <span class="menu-icon fa fa-tachometer"></span></a>
         </li>
         <li class="sidebar-list">
-          <a href="#">Servers <span class="menu-icon glyphicon glyphicon-transfer"></span></a>
+          <a href="#">Servers <span class="menu-icon fa fa-tasks"></span></a>
         </li>
         <li class="sidebar-list">
-          <a href="#">Settings <span class="menu-icon glyphicon glyphicon-transfer"></span></a>
+          <a href="#">Settings <span class="menu-icon fa fa-cogs"></span></a>
         </li>
         <li class="sidebar-list">
           <a href="#">Something <span class="menu-icon glyphicon glyphicon-transfer"></span></a>
         </li>
         <li class="sidebar-title"><span>QUICK LINKS</span></li>
         <li class="sidebar-list">
-          <a href="#">Forums <span class="menu-icon glyphicon glyphicon-dashboard"></span></a>
+          <a href="#">Forums <span class="menu-icon fa fa-external-link"></span></a>
         </li>
       </ul>
     </div>
@@ -52,10 +56,23 @@
           <div class="row">
               <div class="header col-xs-12">
                 <div class="meta pull-left">
-                  Page
+                  <div class="page">
+                    Dashboard
+                  </div>
+                  <div class="breadcrumb-links">
+                    Home / Dashboard
+                  </div>
                 </div>
                 <div class="user pull-right">
-                  User profile
+                  <ul>
+                    <li>A</li>
+                    <li>B</li>
+                    <li>
+                      <a href="#">
+                        <img src="http://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/2b/2b645d34509714b08352da02f13d8db7f0b5c1d5_full.jpg">
+                      </a>
+                    </li>
+                  </ul>
                 </div>
               </div>
           </div>
