0822b2383e81c0685629a8ab154be18fa75e835b
Elliot Hesp <elliot.hesp@gmail.com>
2014-06-24 17:35:06 +0100

    Added sidebar footer

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -31,9 +31,7 @@
             <span class="menu-icon glyphicon glyphicon-transfer"></span>
           </a>
         </li>
-        <li class="sidebar-title">
-          <span>NAVIGATION</span>
-        </li>
+        <li class="sidebar-title"><span>NAVIGATION</span></li>
         <li class="sidebar-list">
           <a href="http://127.0.0.1/git/Sourcemod-Server-Manager-System/public/">Dashboard <span class="menu-icon fa fa-tachometer"></span></a>
         </li>
@@ -51,6 +49,23 @@
           <a href="#">Forums <span class="menu-icon fa fa-external-link"></span></a>
         </li>
       </ul>
+      <div class="sidebar-footer">
+        <div class="col-xs-4">
+          <a href="https://github.com/Ehesp/Sourcemod-Server-Manager-System">
+            Github
+          </a>
+        </div>
+        <div class="col-xs-4">
+          <a href="#">
+            Support
+          </a>
+        </div>
+        <div class="col-xs-4">
+          <a href="#">
+            Steam
+          </a>
+        </div>
+      </div>
     </div>
           
     <!-- Page content -->
