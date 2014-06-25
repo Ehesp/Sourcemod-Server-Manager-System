17053d63c3707d5ac3dbdb4cb5568cae7c5e5d05
Elliot Hesp <elliot.hesp@gmail.com>
2014-06-25 21:40:48 +0100

    Updated sidebar links

--- app/views/partials/sidebar.blade.php
+++ app/views/partials/sidebar.blade.php
@@ -13,14 +13,30 @@
     <a href="#">Servers <span class="menu-icon fa fa-tasks"></span></a>
   </li>
   <li class="sidebar-list">
-    <a href="#">Settings <span class="menu-icon fa fa-cogs"></span></a>
+    <a href="#">Active Plugins <span class="menu-icon fa fa-sitemap"></span></a>
   </li>
   <li class="sidebar-list">
-    <a href="#">Something <span class="menu-icon glyphicon glyphicon-transfer"></span></a>
+    <a href="#">Multi-Console <span class="menu-icon fa fa-terminal"></span></a>
+  </li>
+  <li class="sidebar-list">
+    <a href="#">Admin Activity <span class="menu-icon fa fa-comments"></span></a>
+  </li>
+  <li class="sidebar-list">
+    <a href="#">Game Types <span class="menu-icon fa fa-gamepad"></span></a>
+  </li>
+  <li class="sidebar-list">
+    <a href="#">Settings <span class="menu-icon fa fa-cogs"></span></a>
   </li>
   <li class="sidebar-title"><span>QUICK LINKS</span></li>
   <li class="sidebar-list">
-    <a href="#">Forums <span class="menu-icon fa fa-external-link"></span></a>
+    <a href="#">
+      Forums <span class="menu-icon fa fa-external-link"></span>
+    </a>
+  </li>
+  <li class="sidebar-list">
+    <a href="#">
+      IRC <span class="menu-icon fa fa-comment"></span>
+    </a>
   </li>
 </ul>
 <div class="sidebar-footer">
