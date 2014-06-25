6bb3636765c5f8f4d1c94f9381d4958110f9c808
Elliot Hesp <elliot.hesp@gmail.com>
2014-06-25 21:55:11 +0100

    More language header support

--- app/lang/en/auth.php
+++ app/lang/en/auth.php
@@ -2,6 +2,16 @@
 
 return array(
 
+	/*
+	|--------------------------------------------------------------------------
+	| Authentication Language lines
+	|--------------------------------------------------------------------------
+	|
+	| The below lines are the responses returned when authenicating with the
+	| the application. Responses are returned from the AuthController.
+	|
+	*/
+
 	'invalidResponse' => 'The response from Steam was invalid.',
 
 	'firstLogin' => 'Thanks for using SSMS! Please update any settings below.',
--- app/lang/en/header.php
+++ app/lang/en/header.php
@@ -2,9 +2,18 @@
 
 return array(
 
+	/*
+	|--------------------------------------------------------------------------
+	| Header Language lines
+	|--------------------------------------------------------------------------
+	|
+	*/
+
 	'defaultTitle' => 'Page Title',
 
 	'defaultBreadcrumb' => 'SSMS',
+
+	'yourProfile' => 'Your Profile',
 
 	'steamProfile' => 'Steam Profile',
 
