6026263083c13a9a01eea5bc210602ed9b9d5f7c
Elliot Hesp <elliot.hesp@gmail.com>
2014-06-25 21:40:38 +0100

    Added language support to header

---
+++ app/lang/en/header.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+	'defaultTitle' => 'Page Title',
+
+	'defaultBreadcrumb' => 'SSMS',
+
+	'steamProfile' => 'Steam Profile',
+
+	'steamRep' => 'Steam Rep',
+
+	'logout' => 'Logout',
+
+	'notifications' => 'Notifications',
+
+);
--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -2,10 +2,10 @@
   <div class="col-xs-12">
     <div class="meta pull-left">
       <div class="page">
-        @yield('page_title', 'Page Title')
+        @yield('page_title', Lang::get('header.defaultTitle'))
       </div>
       <div class="breadcrumb-links">
-        @yield('page_breadcrumb', 'Home')
+        @yield('page_breadcrumb', Lang::get('header.defaultBreadcrumb'))
       </div>
     </div>
     @if(Auth::check())
@@ -15,13 +15,26 @@
           <img src="[[ Auth::user()->avatar ]]">
         </a>
         <ul class="dropdown-menu dropdown-menu-right">
-          <li class="dropdown-header">[[ Auth::user()->nickname ]]</li>
-          <li class="divider"></li>
-          <li class="link"><a href="http://steamcommunity.com/profiles/[[ Auth::user()->community_id ]]">Steam Profile</a></li>
-          <li class="link"><a href="#">Steam Rep</a></li>
+          <li class="dropdown-header">
+            [[ Auth::user()->nickname ]]
+          </li>
           <li class="divider"></li>
           <li class="link">
-            <a href="[[ URL::to('logout') ]]">Logout</a></li>
+            <a href="http://steamcommunity.com/profiles/[[ Auth::user()->community_id ]]" target="_blank">
+              @lang('header.steamProfile')
+            </a>
+          </li>
+          <li class="link">
+            <a href="http://steamrep.com/profiles/[[ Auth::user()->community_id ]]" target="_blank">
+              @lang('header.steamRep')
+            </a>
+          </li>
+          <li class="divider"></li>
+          <li class="link">
+            <a href="[[ URL::to('logout') ]]">
+              @lang('header.logout')
+            </a>
+          </li>
         </ul>               
       </div>
       <div class="item dropdown">
@@ -29,7 +42,9 @@
           <i class="fa fa-bell-o"></i>
         </a>
         <ul class="dropdown-menu dropdown-menu-right">
-          <li class="dropdown-header">Dropdown header</li>
+          <li class="dropdown-header">
+            @lang('header.notifications')
+          </li>
           <li>
             <a href="#">Bell</a>
           </li>
