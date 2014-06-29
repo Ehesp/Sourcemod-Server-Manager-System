b212999b73dbf9972897fb9046e8a9049e95b17b
Elliot Hesp <elliot.hesp@gmail.com>
2014-06-29 10:29:34 +0100

    Removed user model boiler plate

--- app/routes.php
+++ app/routes.php
@@ -14,33 +14,6 @@
 Route::get('/', function()
 {
 	return View::make('pages.dashboard');
-
-	/* Attach user to a certain role */
-	// User::find(Auth::user()->id)->roles()->attach(1);
-
-	/* Attach user to a role, method way */
-	// $user = User::find(Auth::user()->id);
-	// $user->assignRole($role) - ID/Object
-
-	/* Get user with their roles */
-	// User::with('roles')->find(Auth::user()->id);
-
-	/* Get user roles only */
-	// User::find(Auth::user()->id)->roles;
-
-	/* Get user roles first name only */
-	// User::find(Auth::user()->id)->roles->first()->name;
-
-	/* Does a user have a certain role? (by name) */
-	// $user = User::find(Auth::user()->id);
-	// if($user->hasRole('super_admin')) return "yep";
-
-	
-
-
-
-	//return User::with('roles')->find(Auth::user()->id);
-
 });
 
 Route::get('settings', ['as' => 'settings', 'uses' => 'SettingController@getView']);
