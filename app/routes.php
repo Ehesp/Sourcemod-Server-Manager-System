8780d1286f803044d7879e3c1a8a2f5ff80ad464
Elliot Hesp <elliot.hesp@gmail.com>
2014-06-30 00:04:11 +0100

    Updated routes

--- app/routes.php
+++ app/routes.php
@@ -2,21 +2,53 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Dashboard redirect
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
+| To validate page access and avoid confusion, we redirect the user
+| to the 'dashboard' route if they hit the document root
 |
 */
 
-Route::get('/', ['as' => 'dashboard', 'uses' => 'DashboardController@getView']);
+Route::get('/', function() { return Redirect::route('dashboard'); });
 
-Route::get('dashboard', ['as' => '/', 'uses' => 'DashboardController@getView']);
+/*
+|--------------------------------------------------------------------------
+| Access Required Routes
+|--------------------------------------------------------------------------
+|
+| These routes first undergo an 'access' filter check, by matching the
+| first segment within the URI with the pages they're allowed to access
+| which are pulled from the IoC container.
+|
+*/
 
-Route::get('settings', ['as' => 'settings', 'uses' => 'SettingController@getView']);
+Route::group(['before' => 'access'], function()
+{
+	Route::get('dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@getView']);
+
+	Route::get('servers', ['as' => 'servers', 'uses' => 'ServerController@getView']);
+
+	Route::get('active-plugins', ['as' => 'active-plugins', 'uses' => 'PluginsController@getView']);
+
+	Route::get('multi-console', ['as' => 'multi-console', 'uses' => 'ConsoleController@getView']);
+
+	Route::get('admin-activity', ['as' => 'admin-activity', 'uses' => 'AdminController@getView']);
+
+	Route::get('game-types', ['as' => 'game-types', 'uses' => 'GamesController@getView']);
+
+	Route::get('settings', ['as' => 'settings', 'uses' => 'SettingController@getView']);
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| Authentication Routes
+|--------------------------------------------------------------------------
+|
+*/
 
 Route::get('login', 'AuthController@validateSteamLogin');
 
 Route::get('logout', 'AuthController@logout');
+
