cc4f90b9b12c89e825237e8673cc2db05b70a214
Erik-jan Riemers <riemers@binkey.nl>
2014-07-09 16:25:39 -0500

    Example steam

--- app/controllers/GameTypeController.php
+++ app/controllers/GameTypeController.php
@@ -4,9 +4,27 @@ class GameTypeController extends BaseController {
 
 	public function getView()
 	{
-        $server = new SourceServer('192.168.0.114', 27015);
+
+        try {
+        error_reporting(0);
+        SteamSocket::setTimeout(2000);
+        $server = new SourceServer('46.4.99.5', 28080);
         $server->initialize();
-        print_r($server->getPlayers());
+        //return $server->getRules();
+            $players = $server->getPlayers(); // Player pings are only available if the RCON password is provided
+            echo "<table>";
+            echo "<tr><th>Name</th><th>Score</th></tr>";
+            foreach($players as $player) {
+                echo "<tr>";
+                echo "<td>{$player->getName()}</td>";
+                echo "<td>{$player->getScore()}</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+
+        } catch (Exceptio $e) {
+            echo "Timeout or cannot connect to server";
+        }
 	}
 
 }
