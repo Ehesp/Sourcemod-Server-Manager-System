bd339b4c17e92ddea0c81f916762efb8a5764e9f
Elliot Hesp <elliot.hesp@gmail.com>
2014-07-14 17:56:45 +0100

    Added quick links editing functionality

---
+++ app/views/pages/settings/quick-links.blade.php
@@ -0,0 +1,102 @@
+@extends('master')
+
+@section('page_title')
+  Quick Links
+@stop
+
+@section('page_breadcrumb')
+  [[ HTML::link('/', 'SSMS') ]] / [[ HTML::link(URL::route('settings'), 'Settings') ]] / [[ HTML::link(URL::route('settings.quick-links'), 'Quick Links') ]]
+@stop
+
+@section('assets')
+  @parent
+  [[ HTML::script('js/angular/controllers/settings/SettingsQuickLinksCtrl.js') ]]
+@stop
+
+@section('content')
+	<div ng-controller="SettingsQuickLinksCtrl">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="widget">
+					<div class="widget-title">
+						<i class="fa fa-external-link-square"></i> Manage Quick Links
+						<input type="text" class="form-control input-sm pull-right" placeholder="Search..." ng-model="searchQuickLinks">
+						<div class="clearfix"></div>
+					</div>
+					<div class="widget-body no-padding">
+
+						<loading ng-if="loading"></loading>
+
+						<div class="error" ng-if="!loading && error">
+							<span ng-bind-html="message"></span>
+						</div>
+
+						<div class="table-responsive" ng-if="!loading && !error">
+							<table class="table">
+								<thead>
+									<tr>
+										<th>Name</th>
+										<th>URL</th>
+										<th>Icon (<a target="_blank" href="http://fontawesome.io/icons/">Font Awesome</a>)</th>
+										<th class="text-right"><i class="fa fa-cog"></i></th>
+									</tr>
+								</thead>
+								<tbody>
+									 <tr ng-repeat="link in links | filter:searchQuickLinks">
+										<td ng-if="!edit[link.id]" ng-bind="link.name"></td>
+										<td ng-if="edit[link.id]">
+											<input class="form-control" type="text" ng-init="link.edit.name = link.name" ng-model="link.edit.name" />
+										</td>
+										<td ng-if="!edit[link.id]" ng-bind="link.url"></td>
+										<td ng-if="edit[link.id]">
+											<input class="form-control" type="text" ng-init="link.edit.url = link.url" ng-model="link.edit.url" />
+										</td>
+										<td ng-if="!edit[link.id]">
+											<i class="{{ link.icon }}"></i>
+										</td>
+										<td ng-if="edit[link.id]">
+											<input class="form-control" type="text" ng-init="link.edit.icon = link.icon" ng-model="link.edit.icon" />
+										</td>
+									 	<td>
+											<button ng-if="!edit[link.id]" class="btn btn-warning btn-sm pull-right" ng-click="editLink(link)" tooltip="Edit Link">
+												<i class="fa fa-wrench"></i>
+											</button>
+											<button ng-if="!edit[link.id]" class="btn btn-danger btn-sm pull-right" ng-click="deleteLink(link)" tooltip="Delete Link">
+												<i class="fa fa-trash-o"></i>
+											</button>
+											<button ng-if="edit[link.id]" class="btn btn-danger btn-sm pull-right" ng-click="editLink(link)" tooltip="Cancel">
+												<i class="fa fa-times"></i>
+											</button>
+											<button ng-if="edit[link.id]" class="btn btn-success btn-sm pull-right" ng-click="saveLink(link)" tooltip="Saves Changes">
+												<i class="fa" ng-class="{'fa-check': !saving[link.id], 'fa-spinner fa-spin': saving[link.id]}"></i>
+											</button>
+									 	</td>
+									</tr>
+									<tr>
+										<td>
+											<input class="form-control input-sm" type="text" ng-model="newlink.name" placeholder="Google" />
+										</td>
+										<td>
+											<input class="form-control input-sm" type="text" ng-model="newlink.url" placeholder="http://google.com" />
+										</td>
+										<td>
+											<input class="form-control input-sm" type="text" ng-model="newlink.icon" placeholder="fa fa-google" />
+										</td>
+										<td>
+											<button class="btn btn-success btn-sm pull-right" ng-click="addLink(newlink)" tooltip="Add Link">
+												<i class="fa" ng-class="{'fa-plus': !adding, 'fa-spinner fa-spin': adding}"></i>
+											</button>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		@include('partials.toaster')
+
+		</div>
+	</div>
+	@stop
---
+++ public/js/angular/controllers/settings/SettingsQuickLinksCtrl.js
@@ -0,0 +1,140 @@
+app.controller('SettingsQuickLinksCtrl', function($scope, $http, dialogs, http, toaster)
+{
+	$scope.loading = true;
+	$scope.links = {};
+	$scope.edit = {};
+	$scope.saving = {};
+	$scope.adding = false;
+
+	/**
+	* Load SSMS quick links
+	*/
+	http.post(window.app_path + 'settings/quick-links').
+		success(function(data)
+		{
+			$scope.links = data;
+			$scope.loading = false;
+		}).
+		error(function(data, status, headers, config)
+		{
+			$scope.message = errorExceptionMessage(data, status, config);
+			$scope.error = true;
+			$scope.loading = false;
+		});
+
+	/**
+	* Edit quick-link
+	*/ 
+	$scope.editLink = function(link)
+	{
+		$scope.edit[link.id] = ! angular.isDefined($scope.edit[link.id]) ? true : ! $scope.edit[link.id];
+	}
+
+	/**
+	* Save quick-link
+	*/ 
+	$scope.saveLink = function(link)
+	{
+		$scope.saving[link.id] = true;
+
+		http.post(window.app_path + 'settings/quick-links/edit', JSON.stringify(link)).
+			success(function(data)
+			{
+				$scope.saving[link.id] = false;
+
+				if(! data.status)
+				{
+					toaster.pop('error', 'Failed to save changes!', '', null, null, function()
+					{
+						dialogs.error('An error occured saving the link changes!', errorMessage(data.code, data.message));
+					});
+				}
+				else
+				{
+					toaster.pop('success', data.message);
+
+					// Update the user before we close the edit area
+					$scope.links[findWithAttr($scope.links, 'id', link.id)] = data.payload;
+
+					$scope.edit[link.id] = false;
+				}
+			}).
+			error(function(data, status, headers, config)
+			{
+				$scope.saving[link.id] = false;
+				dialogs.error('A fatal error occured!', errorExceptionMessage(data, status, config));
+			});
+	}
+
+	/**
+	* Delete quick-link
+	*/ 
+	$scope.deleteLink = function(link)
+	{
+		// Trigger dialog
+		var d = dialogs.confirm('Delete Quick Link - ' + link.name, 'Are you sure you want to delete this quick link?');
+
+		// On dialog "confirm"
+		d.result.then(function(c) {
+			http.post(window.app_path + 'settings/quick-links/delete', link.id).
+				success(function(data)
+				{
+					if(! data.status)
+					{
+						toaster.pop('error', 'Deleting the quick link failed!', '', null, null, function()
+						{
+							dialogs.error('An error occured while deleting the quick link', errorMessage(data.code, data.message));
+						});
+					}
+					else
+					{
+						$scope.links.splice(findWithAttr($scope.links, 'id', link.id), 1);
+						toaster.pop('success', data.message);	
+					}
+				}).
+				error(function(data, status, headers, config)
+				{
+					dialogs.error('A fatal error occured!', errorExceptionMessage(data, status, config));
+				});
+		});
+	}
+
+	/**
+	* Add quick-link
+	*/ 
+	$scope.addLink = function(newlink)
+	{
+		if (angular.isDefined(newlink))
+		{
+			if (angular.isDefined(newlink.name) && angular.isDefined(newlink.url) && angular.isDefined(newlink.icon))
+			{
+				$scope.adding = true;
+
+				http.post(window.app_path + 'settings/quick-links/add', JSON.stringify(newlink)).
+					success(function(data)
+					{
+						$scope.adding = false;
+
+						if(! data.status)
+						{
+							toaster.pop('error', 'Adding the quick link failed!', '', null, null, function()
+							{
+								dialogs.error('An error occured while adding the quick link', errorMessage(data.code, data.message));
+							});
+						}
+						else
+						{
+							$scope.links.push(data.payload);
+							toaster.pop('success', data.message);
+						}
+					}).
+					error(function(data, status, headers, config)
+					{
+						$scope.adding = false;
+						dialogs.error('A fatal error occured!', errorExceptionMessage(data, status, config));
+					});
+			}
+		}
+	}
+
+});
