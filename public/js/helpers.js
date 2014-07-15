f4796ce5e43e00585f739c1e7acb906180defe2b
Elliot Hesp <elliot.hesp@gmail.com>
2014-07-15 12:40:59 +0100

    Added number validate helper

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -28,15 +28,32 @@ function findWithAttr(array, attr, value) {
     }
 }
 
+function validateNumber(num)
+{
+	if (isNaN(parseFloat(num)) && !isFinite(num))
+	{
+		if(! /[^0-9]/.test(num)) return false;
+	}
+
+	return true;
+}
+
 // Validate a string for a valid Steam ID
 function validateSteamData(string)
 {
 	if (! string || /[^a-zA-Z0-9_:]/.test(string)) return false;
 
-	if (isNaN(parseFloat(string)) && !isFinite(string))
+	if (! validateNumber(string))
 	{
 		if(! /^STEAM_[0-5]:[01]:\d+$/.test(string)) return false;
 	}
 
 	return true;
+}
+
+function validateIp(ip)
+{
+	var regexp = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/g;
+
+	return regexp.test(ip);
 }
