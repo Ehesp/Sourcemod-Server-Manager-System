f23cf0d51f6d235f3facf6826325cb58189de535
Elliot Hesp <elliot.hesp@gmail.com>
2014-07-22 22:41:15 +0100

    Added cascade delete back in to role_user table

--- app/database/migrations/2014_06_27_153701_create_role_user_table.php
+++ app/database/migrations/2014_06_27_153701_create_role_user_table.php
@@ -19,7 +19,7 @@ class CreateRoleUserTable extends Migration {
 			$table->integer('user_id')->unsigned()->index();
 			$table->integer('role_id')->unsigned()->index();
 
-			$table->foreign('role_id')->references('id')->on('roles');
+			$table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');;
 			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
 			$table->timestamps();
