b6be35745a75cf6fa216eb05478ad1cc874efa43
Elliot Hesp <elliot.hesp@gmail.com>
2014-07-22 17:32:29 +0100

    Created bindings for interface -> repo mapping

--- app/bindings.php
+++ app/bindings.php
@@ -1,5 +1,9 @@
 <?php
 
-// IoC Bindings
-
 App::bind('Ssms\Repositories\User\UserRepositoryInterface', 'Ssms\Repositories\User\EloquentUserRepository');
+
+App::bind('Ssms\Repositories\Role\RoleRepositoryInterface', 'Ssms\Repositories\Role\EloquentRoleRepository');
+
+App::bind('Ssms\Repositories\Permission\PermissionRepositoryInterface', 'Ssms\Repositories\Permission\EloquentPermissionRepository');
+
+App::bind('Ssms\Repositories\Option\OptionRepositoryInterface', 'Ssms\Repositories\Option\EloquentOptionRepository');
