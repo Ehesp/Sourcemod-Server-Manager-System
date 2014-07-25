3c55fdeac25bd390babc91a4a1060d58913aa1ed
Elliot Hesp <elliot.hesp@gmail.com>
2014-07-25 18:13:59 +0100

    Changed binding names to reflect repoistories

--- app/bindings.php
+++ app/bindings.php
@@ -1,9 +1,11 @@
 <?php
 
-App::bind('Ssms\Repositories\User\UserRepositoryInterface', 'Ssms\Repositories\User\EloquentUserRepository');
+App::bind('Ssms\Repositories\User\UserRepository', 'Ssms\Repositories\User\EloquentUserRepository');
 
-App::bind('Ssms\Repositories\Role\RoleRepositoryInterface', 'Ssms\Repositories\Role\EloquentRoleRepository');
+App::bind('Ssms\Repositories\Role\RoleRepository', 'Ssms\Repositories\Role\EloquentRoleRepository');
 
-App::bind('Ssms\Repositories\Permission\PermissionRepositoryInterface', 'Ssms\Repositories\Permission\EloquentPermissionRepository');
+App::bind('Ssms\Repositories\Permission\PermissionRepository', 'Ssms\Repositories\Permission\EloquentPermissionRepository');
 
-App::bind('Ssms\Repositories\Option\OptionRepositoryInterface', 'Ssms\Repositories\Option\EloquentOptionRepository');
+App::bind('Ssms\Repositories\Option\OptionRepository', 'Ssms\Repositories\Option\EloquentOptionRepository');
+
+App::bind('Ssms\Repositories\QuickLink\QuickLinkRepository', 'Ssms\Repositories\QuickLink\EloquentQuickLinkRepository');
